1cbb6247c1c6aa0d4c442c494372a76dcc783ead
HiTechGitHub <vgorbanev702@gmail.com>
2023-04-10 18:50:10 +0300

    proverka

--- mathOperation/Calculator.java
+++ mathOperation/Calculator.java
@@ -4,18 +4,23 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.function.BinaryOperator;
 
-public class Calculator {
+public class Calculator{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("делаем умножение на 2 числа?:");
+        System.out.println("делаем умножение на 2 числа и после сумму 2 чисел?:");
         String doIt = in.nextLine();
         if (doIt.equals("да")) {
-            System.out.println("Ожидание числа x...");
+            System.out.println("Ожидание числа x1...");
             int xMulti = in.nextInt();
-            System.out.println("Ожидание числа y...");
+            System.out.println("Ожидание числа y1...");
             int yMulti = in.nextInt();
+            System.out.println("Ожидание числа x2...");
+            int xPlus = in.nextInt();
+            System.out.println("Ожидание числа y2...");
+            int yPlus = in.nextInt();
             BinaryOperator<Integer> multiplication = (x, y) -> (x * y);
-            System.out.println(multiplication.apply(xMulti, yMulti));
+            BinaryOperator<Integer> plus = Integer::sum;
+            System.out.println(multiplication.apply(xMulti, yMulti) + plus.apply(xPlus, yPlus));
         }
     }
 
